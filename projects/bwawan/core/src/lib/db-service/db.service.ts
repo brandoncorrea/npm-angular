0aa989b37d5231a7a726d0fc14a2db77a031dee8
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2023-04-25 19:05:02 -0400

    Stop caring about types

--- projects/bwawan/core/src/lib/db-service/db.service.ts
+++ projects/bwawan/core/src/lib/db-service/db.service.ts
@@ -11,23 +11,23 @@ function isSuperset(parent: any, child: any) {
 export class DbService {
   private entities: Map<number, Object> = new Map()
 
-  saveAll = (entities: any[]) => this.save(...entities)
-  save(...entities: any[]) {
+  saveAll = (entities: any[]): void => this.save(...entities)
+  save(...entities: any[]): void {
     for(var entity of entities) {
       if (!entity?.id) throw 'missing id'
       this.entities.set(entity.id, entity)
     }
   }
 
-  firstBy = (entity: any) => this.firstWhere((e: any) => isSuperset(e, entity))
-  firstWhere(pred: any) {
+  firstBy = (entity: any): any => this.firstWhere((e: any) => isSuperset(e, entity))
+  firstWhere(pred: any): any {
     for (var entity of this.entities.values())
     if (pred(entity))
     return entity
     return null
   }
   
-  countWhere = (pred: any) => {
+  countWhere = (pred: any): number => {
     let count = 0
     for (var entity of this.entities.values())
     if (pred(entity))
@@ -35,10 +35,10 @@ export class DbService {
     return count
   }
   
-  all = () => Array.from(this.entities.values())
-  get = (id: any) => this.entities.get(id) || null
-  delete = (entityOrId: any) => this.entities.delete(entityOrId.id || entityOrId)
-  findWhere = (pred: any) => this.all().filter(pred)
-  findBy = (entity: any) => this.findWhere((e: any) => isSuperset(e, entity))
-  countBy = (entity: any) => this.countWhere((e: any) => isSuperset(e, entity))
+  all = (): any[] => Array.from(this.entities.values())
+  get = (id: any): any => this.entities.get(id) || null
+  delete = (entityOrId: any): boolean => this.entities.delete(entityOrId.id || entityOrId)
+  findWhere = (pred: any): any[] => this.all().filter(pred)
+  findBy = (entity: any): any[] => this.findWhere((e: any) => isSuperset(e, entity))
+  countBy = (entity: any): number => this.countWhere((e: any) => isSuperset(e, entity))
 }
